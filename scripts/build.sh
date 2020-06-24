757012cd757c48bf9fbb3805aa39ee9fc0a39282
Vinicius Isola <viniciusisola@gmail.com>
2020-06-24 07:26:40 -0400

    Change script to generate all diagrams

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-npx -p mermaid.cli mmdc \
-  -i applications/photos/fe_or_be_change_deployment.mermaid \
-  -o applications/photos/diagrams/fe_or_be_change_deployment.png
+find . -name "*.mermaid" -exec ./scripts/convert_diagram.sh {} \;
